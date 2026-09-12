d7c1f4f23d2fe1ec06da9dd04cd3e36cdffd0fdd
yoiyoip123 <yoiyoip123@gmail.com>
2026-09-12 12:07:34 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+// 여행 플래너 서비스워커 - 오프라인에서도 앱이 열리게 함
+const CACHE = 'travel-planner-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+// 설치: 핵심 파일 캐시
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+// 활성화: 오래된 캐시 정리
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 요청 처리: 네트워크 우선, 실패 시 캐시 (항상 최신 일정 우선)
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((r) => r || caches.match('./index.html')))
+  );
+});
